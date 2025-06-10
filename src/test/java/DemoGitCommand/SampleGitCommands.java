fb004b9ff9d3bf08d1ff2e2785acd817082f2d29
unknown <rohinigururaj10063@gmail.com>
2025-06-10 17:12:12 +0530

    rohiniprojectcommit

---
+++ src/test/java/DemoGitCommand/SampleGitCommands.java
@@ -0,0 +1,12 @@
+package DemoGitCommand;
+
+import java.lang.invoke.MethodHandles.Lookup.ClassOption;
+
+import org.testng.annotations.Test;
+
+public class SampleGitCommands {
+@Test
+public void readDataTest() {
+	System.out.println("hi");
+}
+}
